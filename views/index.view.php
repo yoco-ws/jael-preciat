fbffce63175b2784f156162608f38876d2c2b14d
Irving R. Ché <che.chan.irving@gmail.com>
2020-12-22 23:07:51 -0600

    index title

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<title>Jael Preciat, Dot-Connetor</title>
+		<title>Jael Preciat, Dot-Connector</title>
 
 		<meta property="og:url"                content="<?php echo $URL; ?>" />
 		<meta property="og:type"               content="website" />
